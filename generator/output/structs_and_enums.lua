11cfcab37eb5c9ae7dbf3eff7d8932cf25ad1345
sonoro1234 <sonoroster@gmail.com>
2025-11-20 12:11:25 +0100

    regenerate for 1.92.5wip

--- generator/output/structs_and_enums.lua
+++ generator/output/structs_and_enums.lua
@@ -104,6 +104,7 @@ local t={
     ImGuiContext="inherited",
     ImGuiContextHook="inherited",
     ImGuiDebugAllocInfo="inherited",
+    ImGuiDebugItemPathQuery="inherited",
     ImGuiDockContext="inherited",
     ImGuiDockNode="inherited",
     ImGuiErrorRecoveryState="inherited",
